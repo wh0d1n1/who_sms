8046f8ec6028d646d1b5b8ee2a229cc863188fc5
Travis Padilla <travispadilla112@gmail.com>
2023-01-16 02:54:38 -0500

    'a'

--- webpack/config.js
+++ webpack/config.js
@@ -66,7 +66,7 @@ const config = {
           test: /\.m?js/,
           resolve: {
             fullySpecified: false
-          }
+          },
           exclude: /node_modules/
       },
       {

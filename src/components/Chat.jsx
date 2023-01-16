3b25dc264b22c1e19dc9abbc722f7d09532c7b0a
Travis Padilla <travispadilla112@gmail.com>
2023-01-15 21:44:29 -0500

    'a'

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,16 +2,16 @@ import { useEffect } from 'react';
 // @mui
 import { Card, Container } from '@mui/material';
 // redux
-import { useDispatch } from '../../redux/store';
-import { getConversations, getContacts } from '../../redux/slices/chat';
+import { useDispatch } from '../redux/store';
+import { getConversations, getContacts } from '../redux/slices/chat';
 // routes
-import { PATH_DASHBOARD } from '../../routes/paths';
+import { PATH_DASHBOARD } from '../routes/paths';
 // hooks
-import useSettings from '../../hooks/useSettings';
+import useSettings from '../hooks/useSettings';
 // components
-import Page from '../../components/Page';
-import HeaderBreadcrumbs from '../../components/HeaderBreadcrumbs';
-import { ChatSidebar, ChatWindow } from '../../sections/@dashboard/chat';
+import Page from '../components/Page';
+import HeaderBreadcrumbs from '../components/HeaderBreadcrumbs';
+import { ChatSidebar, ChatWindow } from '../sections/@dashboard/chat';
 
 // ----------------------------------------------------------------------
 
